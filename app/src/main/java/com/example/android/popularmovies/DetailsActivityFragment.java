10bcf304fd927759b53b12b60156204e08a17159
Aiman <aiman.nabeel@gmail.com>
2018-04-05 15:34:30 +0100

    Corrections made in MoviesAdapter, DetailActivity & Movie class

---
+++ app/src/main/java/com/example/android/popularmovies/DetailsActivityFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.popularmovies;
+
+import android.app.Fragment;
+
+
+public class DetailsActivityFragment extends Fragment {
+
+    /*final String BASE_URL = "http://api.themoviedb.org/3/movie/";
+
+    //Remove this key before uploading the project
+    String api_key_value = "12356789";
+    final int PURPOSE_TRAILER = 1;
+    final int PURPOSE_REVIEWS = 2;
+    final String TRAILER_QUERY = "/videos?";
+    final String REVIEW_QUERY = "/reviews?";
+
+    private static String TAG = DetailsActivityFragment.class.getSimpleName();
+
+    public DetailsActivityFragment() {
+        setHasOptionsMenu(true);
+    }*/
+
+
+
+}
